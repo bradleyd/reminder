b775eff6aff2bf0b5be69dd3311e9b56e7eb4f83
Bradley Smith <bradleydsmith@gmail.com>
2015-01-10 13:45:41 -0700

    add #start, #init

--- lib/reminder/event.ex
+++ lib/reminder/event.ex
@@ -3,13 +3,44 @@ defmodule Reminder.Event do
     defstruct [:server, name: "", to_go: 0]
   end
 
-  def loop(s = %Reminder.Event.State{server: server, to_go: n}) do
-    receive do
-      {server, ref, cancel} ->
-      send(server, {ref, :ok, "true"})
-    after 
-      (n * 1000) ->
-        send(server, {:done, s.name})
+  def start(event_name, delay) do
+    spawn(__MODULE__, :init, [self(), event_name, delay])
+  end
+
+  def start_link(event_name, delay) do
+    spawn_link(__MODULE__, :init, [self(), event_name, delay]) 
+  end
+
+  def init(server, event_name, delay) do
+    loop(%Reminder.Event.State{server: server, name: event_name, to_go: normalize(delay)})
+  end
+
+  def cancel(pid) do
+    case Process.alive?(pid) do
+      true ->
+        {:ok}
+      false ->
+        {:ok, "down"}
     end
+  end
+
+  def loop(s = %Reminder.Event.State{server: server, to_go: [t|n]}) do
+    receive do
+      {server, ref, :cancel} ->
+        send(server, {ref, :ok, "true"})
+    after 
+      (t * 1000) ->
+        cond do
+          n == [] ->
+            send(server, {:done, s.name})
+          n != [] ->
+            loop(%{s | to_go: n })
+        end
+    end
+  end
+
+  defp normalize(n) do
+    limit = 49*24*60*60
+    [rem(n, limit) | List.duplicate(n, div(limit, limit))]
   end
 end
